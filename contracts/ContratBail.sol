85d9d4cd74e7905b04bcdd72d5ff5bb82def7d38
Tahirou Magagi <65642962+latomate07@users.noreply.github.com>
2026-04-22 14:45:04 +0200

    Add payerLoyer() method

--- contracts/ContratBail.sol
+++ contracts/ContratBail.sol
@@ -18,4 +18,16 @@ contract ContratBail {
         proprietaire = msg.sender;
         loyer = _loyer;
     }
+
+    function definirLocataire(address _locataire) public {
+        require(msg.sender == proprietaire, "Seul le proprietaire peut definir le locataire");
+        locataire = _locataire;
+    }
+
+    // ---------- Part 5 : le paiement du loyer ----------
+    function payerLoyer() public payable {
+        require(msg.sender == locataire, "Seul le locataire peut payer le loyer");
+        require(msg.value == loyer, "Montant incorrect");
+        loyerPaye = true;
+    }
 }
