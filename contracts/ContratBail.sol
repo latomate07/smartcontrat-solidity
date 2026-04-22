e20839012e2a3368f7703623ce540feb6bc85149
Vincent7518 <mail.vincent75@gmail.com>
2026-04-22 14:31:17 +0200

    update

--- contracts/ContratBail.sol
+++ contracts/ContratBail.sol
@@ -1,17 +1,21 @@
 pragma solidity ^0.8.0;
+
 contract ContratBail {
+    // ---------- Part 1 ----------
     string public nomContrat = "Contrat de Bail avec Blockchain";
+
+    // ---------- Part 2 : les acteurs ----------
     address public proprietaire;
     address public locataire;
+
+    // ---------- Part 3 : le loyer ----------
     uint public loyer;
 
-    constructor (address _locataire) {
+    // ---------- Constructor (Parts 2 & 3) ----------
+    // Le proprietaire = celui qui deploie le contrat
+    // Le loyer est defini au moment du deploiement
+    constructor(uint _loyer) {
         proprietaire = msg.sender;
-        locataire = _locataire;
-    }
-
-    function definirLocataire(address locataire) public{
-        
+        loyer = _loyer;
     }
 }
-
